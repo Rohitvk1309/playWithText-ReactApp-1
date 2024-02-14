94f87f45bca5f5d60bab3e7f5e4ecbc12f859e29
Rohitvk1309 <rohitis.1309@gmail.com>
2024-02-14 08:27:22 +0530

    1 st

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './Appa.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
-      <Navbar title="Textutils2" AboutText="About us" />
+      {/* <Navbar title="Textutils2" AboutText="About us" /> */}
       {/* <Navbar/> */}
       <div className="container my-3">
       <TextForm heading = "Enter the text area"/>
+      <About/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,22 +7,36 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoclick =()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
 
     const handleOnchange = (event) =>{
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("Enter here");
+    const [text, setText] = useState("");
     // Text = "hero"  // wrong way to change the text
     // setText("new Text");
 
     return (
-        <div>
+        <>
+        <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoclick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+        <h2>Your Text summary</h2>
+        <p>{(text.split(" ").length)-1} words,{text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read characters</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
